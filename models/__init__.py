77493071d7672321af38db16c873c5a222fad46c
Iga-CML_Linux <iga.narendra@gmail.com>
2024-01-09 22:16:17 +0900

    add modification files and other new files from figo

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
-from .openpose_singlenet import create_openpose_singlenet
-from .openpose_2br_vgg import create_openpose_2branches_vgg
-
+# from .openpose_singlenet import create_openpose_singlenet
+# from .openpose_2br_vgg import create_openpose_2branches_vgg
+from .modify_model_2br_vgg import create_openpose_2branches_vgg #new modify
+from .modify_model_singlenet import create_openpose_singlenet #new modify
--- models/openpose_singlenet.py
+++ models/openpose_singlenet.py
@@ -12,52 +12,52 @@ class OpenPoseSingleNet(tf.keras.Model):
         ],
 
         # Mobilenet v3 backbone
-
+        # i try to decrypt author means for the model he made -f
         'backbone#': [
             ['select:img#'],
-            ['cn_bn_r1_k3_s2_c16_nhs'],
-            ['ir_r1_k3_s1_e1_c16_nre'],
-            ['ir_r1_k3_s2_e4_c24_nre', 'ir_r1_k3_s1_e3_c24_nre'],
-            ['c3#ir_r3_k5_s2_e3_c40_se4_nre'],
-            ['ir_r1_k3_s2_e6_c80_nhs'],
-            ['ir_r1_k3_s1_e2.5_c80_nhs'],
-            ['ir_r2_k3_s1_e2.3_c80_nhs'],
-            ['c4#ir_r2_k3_s1_e6_c112_se4_nhs'],
-            ['upscaled_c4#up_x2:'],
-            ['cnct:c3#:upscaled_c4#']
+            ['cn_bn_r1_k3_s2_c16_nhs'],#convBnAct(?), layer, kernel 3, stride 2,  
+            ['ir_r1_k3_s1_e1_c16_nre'],#inverted residual kernel 3, stride 1 expansion (dim?) 1 , activation relu
+            ['ir_r1_k3_s2_e4_c24_nre', 'ir_r1_k3_s1_e3_c24_nre'],#
+            ['c3#ir_r3_k5_s2_e3_c40_se4_nre'],#custom name c3# for the last repeated layer. inverted residual 3 layer, kernel 5, stride, relu
+            ['ir_r1_k3_s2_e6_c80_nhs'],#inverted residual 1 layer, kernel 3, stride 2, exp_ratio, hard swish
+            ['ir_r1_k3_s1_e2.5_c80_nhs'],#inverted residual 1 layer, kernel 3, stride 1, expansion 2.5, hard swish
+            ['ir_r2_k3_s1_e2.3_c80_nhs'],#inverted residual 2 layer, kernel 3, stride (1,1), hard swish
+            ['c4#ir_r2_k3_s1_e6_c112_se4_nhs'],# custom name c4# for the last repeated layer. inverted residual 2 layer, kernel 3, stride 1, expansion 6, hard swish 
+            ['upscaled_c4#up_x2:'],#
+            ['cnct:c3#:upscaled_c4#']#
         ],
 
         # PAF stages
 
         'stage_0#': [
             ['select:backbone#'],
-            ['cn2_r5_e1_c192_npre'],
-            ['ir_r1_k1_s1_e1_c256_se4_npre'],
-            ['hd_r1_k1_s1_c38']
+            ['cn2_r5_e1_c192_npre'],#smallerConv7x7, 5 layer, expansion 1, out_chs 192
+            ['ir_r1_k1_s1_e1_c256_se4_npre'],#inverted residual, 1 layer, exp_ratio 1, out_chs 256, se_factor 4
+            ['hd_r1_k1_s1_c38']#tf.keras.layers.conv2d, 1 layer, kernel 1, stride 1, out_chs 38 
         ],
         'stage_1#': [
-            ['select:stage_0#:backbone#'],
-            ['cnct:'],
-            ['cn2_r5_e1_c384_npre'],
-            ['ir_r1_k1_s1_e1_c256_se4_npre'],
-            ['hd_r1_k1_s1_c38']
+            ['select:stage_0#:backbone#'],#after stage_0 finish
+            ['cnct:'],#concatenate
+            ['cn2_r5_e1_c384_npre'],#smallerConv7x7, 5 layer, exp_ratio 1, out_chs 384
+            ['ir_r1_k1_s1_e1_c256_se4_npre'],#inverted residual, 1 layer, kernel 1, stride 1, exp_ratio 1, out_chs 256, se_factor 4(?), PReLu activation(?)
+            ['hd_r1_k1_s1_c38']#tf.keras.layers.Conv2D, 1 layer, stride 1, out_chs 38
         ],
         'stage_2#': [
-            ['select:stage_1#:backbone#'],
-            ['cnct:'],
-            ['cn2_r5_e1_c384_npre'],
-            ['ir_r1_k1_s1_e1_c512_se4_npre'],
-            ['hd_r1_k1_s1_c38']
+            ['select:stage_1#:backbone#'],#after stage_1 finish
+            ['cnct:'],#concatenate
+            ['cn2_r5_e1_c384_npre'],#smaller conv7x7, 5 layer, exp_ratio 1, out_chs 384, PReLu activation(?)
+            ['ir_r1_k1_s1_e1_c512_se4_npre'],#inverted residual, 1 layer, kernel 1, stride 1, exp_ratio 1, out_chs 512, se_factor 4, PReLu activation(?)
+            ['hd_r1_k1_s1_c38']#tf.keras.layers, 1 layer, kernel 1, stride 1,out_chs 38
         ],
 
         # Heatmap stages
 
         'stage_3#': [
-            ['select:stage_2#:backbone#'],
-            ['cnct:'],
-            ['cn2_r5_e1_c384_npre'],
-            ['ir_r1_k1_s1_e1_c512_se4_npre'],
-            ['hd_r1_k1_s1_c19']
+            ['select:stage_2#:backbone#'], # after stage 2 finish
+            ['cnct:'],#concatenate
+            ['cn2_r5_e1_c384_npre'],#smaller conv7x7, 5 layer, exp_ratio 1, out_chs 384, PReLu activation(?)
+            ['ir_r1_k1_s1_e1_c512_se4_npre'],#inverted residual, 1 layer, kernel 1, stride 1, exp_ratio 1, out_chs 512, se_factor 4, PReLu activation(?)
+            ['hd_r1_k1_s1_c19']#tf.keras.layers, 1 layer, kernel 1, stride 1,out_chs 19
         ],
     }
 
